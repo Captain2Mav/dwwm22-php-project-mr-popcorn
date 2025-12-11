979427e457383c5d9167b39ccaa951f7160afa04
Lenny Zepho <lennyzepho92@gmail.com>
2025-12-11 16:59:39 +0100

    CRUD

---
+++ partials/footer.php
@@ -0,0 +1,4 @@
+ <!-- Footer -->
+    <footer>
+        <p class="text-center">Crée <span class="text-danger">&#9829;</span> par les Dwwm22</p>
+    </footer>
---
+++ public/create.php
@@ -0,0 +1,16 @@
+<?php include_once __DIR__ . "/../partials/head.php"; ?>
+
+<?php include_once __DIR__ . "/../partials/nav.php"; ?>
+
+
+   <!-- Main: Le contenu spécifique à cette page -->
+     <main class="container">
+       <h1 class="text-center my-3 display-5">Nouveau film</h1>
+      
+    <!-- Formulaire d'ajout d'un nouveau film-->
+   
+     </main>
+  
+<?php include_once __DIR__ . "/../partials/footer.php"; ?>
+
+<?php include_once __DIR__ . "/../partials/foot.php"; ?>
